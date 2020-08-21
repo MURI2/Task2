f7c4f0a7abe2505da8c88865feb8af1232c6ee73
Will Shoemaker <shoemakah@gmail.com>
2020-08-20 18:42:13 -0700

    cleanup old mac

--- LTDE/bin/tree/raxml.sh
+++
@@ -1,8 +0,0 @@
-raxmlHPC-PTHREADS -T 4 -m GTRCAT -p 12345 -x 12345 -# 300 \
-  -s /Users/WRShoemaker/github/Task2/LTDE/data/Tree/deathcurves.clustal.afa.fasta \
-  -n T14 \
-  -w /Users/WRShoemaker/github/Task2/LTDE/data/Tree
-
-raxmlHPC-PTHREADS -T 4 -m GTRCAT -J MRE -# 100 \
-  -z /Users/WRShoemaker/github/Task2/LTDE/data/Tree/RAxML_bootstrap.T14 \
-  -n T18 -w /Users/WRShoemaker/github/Task2/LTDE/data/Tree
